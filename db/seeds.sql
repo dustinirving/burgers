c6cd6f0db26aa952182548fab7e98b7be83f7824
Dustin Irving <dustin.irving@gmail.com>
2020-05-06 11:02:16 -0400

    Set up working environment

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+VALUES ('cheeseburger')
+INSERT INTO burgers (burger_name)
+VALUES ('chicken burger')
+INSERT INTO burgers (burger_name)
+VALUES ('bacon cheeseburger')
