9fa5c30b5d82895f253c3ff2fe93c9fce84f95e1
Dustin Irving <dustin.irving@gmail.com>
2020-05-06 11:20:10 -0400

    Set up the server

---
+++ server.js
@@ -0,0 +1,26 @@
+// Get access to the dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+// Create an instance of the express app
+const app = express();
+
+// This serves the static content
+app.use(express.static("public"));
+
+// Handling data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Using the Handlebars template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Route handling
+// app.use(require("./controllers/burgers_controllers"));
+
+// Setting the port for the deployed version or local instance
+const PORT = process.env.PORT || 3000;
+
+// Start the server to listen for client requests
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
