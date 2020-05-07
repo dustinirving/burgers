64d84c02ef60d9a0d3e2e4ee3af9f78fa12fccbf
Dustin Irving <dustin.irving@gmail.com>
2020-05-07 12:33:47 -0400

    Changed the connection settings for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // Get access to the mysql npm packages
 const mysql = require("mysql2");
+let connection;
 
 // Set the values for the connection to the database
-const defaultConfig = {
-  host: "localhost",
-  user: "root",
-  password: "hyeonmi91",
-  database: "burgers_db",
-};
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hyeonmi91",
+    database: "burgers_db",
+  });
+}
+
+connection.connect();
 
 // Export the connection as a promise
-module.exports = mysql.createConnection(defaultConfig).promise();
+
+module.exports = connection;
